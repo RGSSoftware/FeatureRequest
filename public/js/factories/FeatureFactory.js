4af35bc50c9de1ddacee220bb222d9862f70399a
Randel Smith <homankids@gmail.com>
2016-04-08 09:24:14 -0500

    Refactored FeatureFactory

--- public/js/factories/FeatureFactory.js
+++ public/js/factories/FeatureFactory.js
@@ -1,11 +1,8 @@
 angular.module('featureFactory', [])
-
-	// super simple service
-	// each function returns a promise object 
+ 
 	.factory('FeatureFactory', ['$http',function($http) {
 		return {
 			getAll : function() {
-				
 				return $http.get('/api/features');
 			}
 			
